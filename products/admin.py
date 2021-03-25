d54a78bbbbe29e861d2866d7dfefe8ff3d239d0e
Stuart Saint <stuartsaint242@gmail.com>
2021-03-25 16:25:54 +0000

    Admin: added classes for product and category admin pages

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Product, Category
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'category',
+        'name',
+        'price',
+        'image',
+    )
+
+    ordering = ('name', 'category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'render_name',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+
