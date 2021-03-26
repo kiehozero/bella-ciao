de7b8714f446593fb94954cefad766b04b361c92
Stuart Saint <stuartsaint242@gmail.com>
2021-03-26 16:56:09 +0000

    Testing: search functionality display query as None

--- products/views.py
+++ products/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render, reverse
 from .models import Product
 
 
 def all_products(request):
     """ Display all items """
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'search' in request.GET['search']:
+            query = request.GET['search']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria.")
+                return redirect(reverse('products'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
