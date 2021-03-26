72e5018f8d5e50641f9c5fddb8169453e7d502d2
Stuart Saint <stuartsaint242@gmail.com>
2021-03-26 22:26:54 +0000

    Addition: search and filter functioning, needs hover/focus/active styling

--- products/views.py
+++ products/views.py
@@ -1,15 +1,21 @@
 from django.contrib import messages
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render, reverse
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
     """ Display all items """
     products = Product.objects.all()
-    query = None
+    categories = None
+    query = None  # defined as none so the page doesn't error out on load
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'search' in request.GET:
             query = request.GET['search']
             if not query:
@@ -17,12 +23,17 @@ def all_products(request):
                     request, "You didn't enter any search criteria.")
                 return redirect(reverse('products'))
 
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        products = products.filter(queries)
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
+
+    # will need to go back to MS3 to find out how to
+    # reset the search criteria to show all products
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
