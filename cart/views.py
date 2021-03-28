11f07ae89ae382018479e8e4f329ac494b800341
Stuart Saint <stuartsaint242@gmail.com>
2021-03-28 19:17:57 +0000

    Addition: cart functionality, items displaying in console

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def view_cart(request):
     """ See the contents (if any) of the shopping cart """
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add quantity of given item to cart. If a cart
+    exists, it updates it, if not it creates it """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
