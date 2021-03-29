01647bfb80309aa4851c867a117b29a56d5c1afd
Stuart Saint <stuartsaint242@gmail.com>
2021-03-29 21:06:24 +0000

    Addition: loyalty, size and flavour logic, contexts and view to cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -9,17 +9,24 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    loyalty_stamps = 0
     cart = request.session.get('cart', {})
 
     # this the loop that adds up the quantity and value of items in the cart
+    # need to include a calc that gets product
+    # loyalty and adds free item if present
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
         total += quantity * product.price
+        subtotal = quantity * product.price
+        if product.loyalty is True:
+            loyalty_stamps += 1
         product_count += quantity
         cart_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
+            'subtotal': subtotal,
         })
 
     # replicate below logic to tell customer how many coffees they have
@@ -43,6 +50,7 @@ def cart_contents(request):
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
+        'loyalty_stamps': loyalty_stamps,
     }
 
     return context
