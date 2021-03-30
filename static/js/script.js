7b01111c92eb11344fa7a4f1b9f804fca15defbe
Stuart Saint <stuartsaint242@gmail.com>
2021-03-30 21:53:26 +0000

    Modify: clean up checkout layout, removed SKU and re-aligned responsive colspan jQ.

--- static/js/script.js
+++ static/js/script.js
@@ -28,5 +28,5 @@ if ($(window).width() < 1280) {
     $("#searchButton").html("<i class='fas fa-search' aria-label='Search Products'></i>");
     $("#searchReset").html("<i class='fas fa-power-off' aria-label='Reset Search Criteria'></i>");
     /* does cart-total change only need to happen on mobile screens? */
-    $(".cart-total").attr("colspan","4");
+    $(".cart-total").attr("colspan","3");
 };
