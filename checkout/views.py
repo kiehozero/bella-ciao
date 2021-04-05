d3779d1366597e6f593e14869732cdb0789352fa
Stuart Saint <stuartsaint242@gmail.com>
2021-04-05 22:45:27 +0000

    Addition: Stripe setup, key scrubbing and context handling

--- checkout/views.py
+++ checkout/views.py
@@ -27,13 +27,12 @@ def checkout(request):
         amount=stripe_total,
         currency=settings.STRIPE_CURRENCY
     )
-    print(intent)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': 'test_client_secret',
+        'client_secret': intent.client_secret,
     }
     return render(request, template, context)
