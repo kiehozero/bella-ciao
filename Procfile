cdab2864bd20d480bd5131bab9819d170131e0cd
Stuart Saint <stuartsaint242@gmail.com>
2021-04-13 16:32:08 +0000

    Deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bella_ciao.wsgi:application
--- bella_ciao/settings.py
+++ bella_ciao/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cafe-bella-ciao.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -133,12 +133,17 @@ WSGI_APPLICATION = 'bella_ciao.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
