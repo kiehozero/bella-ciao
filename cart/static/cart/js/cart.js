cfffbf76afb7b9a51c114e3af16c90814cdfc5e0
Stuart Saint <stuartsaint242@gmail.com>
2021-04-14 18:17:10 +0000

    Styling: clean up on responsive shopping cart elements

--- cart/static/cart/js/cart.js
+++ cart/static/cart/js/cart.js
@@ -2,3 +2,4 @@ if ($(window).width() < 1280) {
     /* does cart-total change only need to happen on mobile screens? */
     $(".cart-total").attr("colspan","3");
 };
+
--- cart/templatetags/cart_tools.py
+++ cart/templatetags/cart_tools.py
@@ -7,3 +7,5 @@ register = template.Library()
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
+
+# add loyalty cart tool here?
