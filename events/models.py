71b2877fd430d79a6e3466aedb0d057c6891ac8a
Stuart Saint <stuartsaint242@gmail.com>
2021-04-15 01:45:28 +0000

    Prep: events model, urls, views ideas and workflows

--- events/models.py
+++ events/models.py
@@ -1,5 +1,26 @@
 from django.db import models
 
-# Event Model
+# class Event(models.Model):
 
-# Event Attendee Model
+### event_name = models.CharField(max_length=30, null=False, blank=False)
+### location = models.CharField(max_length=30, null=False, blank=False)
+### category = models.CharField(max_length=15, null=False, blank=False)
+### description = models.CharField(max_length=500, null=False, blank=False)
+### date = models.DateFimeField(null=True, blank=True)
+### capacity = models.IntegerField(null=False, blank=False, default=0)
+### image = models.ImageField(null=True, blank=True)
+
+### use calendar picker for filling out date
+### display generic CBC logo if no image is present
+### in join_event, if the Event Attendees already contains this number
+### of attendees defined in capacity, users will get a Sold Out message)
+
+# class EventAttendees(models.Model):
+
+### username = models.CharField(max_length=30, null=False, blank=False)
+### event = models.CharField(max_length=30, null=False, blank=False)
+
+### join_event view adds both the user's username and the event item requested
+
+### to view their own events, user profile will need to import EventAttendees and
+### return any entries where a matching username is found
--- events/views.py
+++ events/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+# from django.shortcuts import get_object_or_404, render
+# from .models import Events, EventAttendees
+# # add forms.py to create new events
 
 
-def view_events(request):
-    """ Display list of events """
-    return render(request, 'events/events.html')
+# def view_events(request):
+#     """ Display list of events """
+#     return render(request, 'events/events.html')
+
+
+# def event_detail(request, event_id):
+#     """ Display more information to the user """
+#     """ Display an admin panel for admins """
+#     event = get_object_or_404(Events, event_id=event_id)
+#     template = 'events/event_detail.html'
+#     context = {
+#         'event': event,
+#     }
+#     return render(request, template, context)
+
+
+# def join_event(request, event_id):
+#     template = 'events/join_event.html'
+#     context = {
+#         'event': event,
+#     }
+#     return render(request, template, context)
+
+# def edit_event(request, event_id):
+#     template = 'events/edit_event.html'
+#     context = {
+#         'event': event,
+#     }
+#     return render(request, template, context)
+
+# def delete_event(request, event_id):
+#     template = 'events/delete_event.html'
+#     context = {
+#         'event': event,
+#     }
+#     return render(request, template, context)
