41180081067707537328275073317b540cb7ae60
Stuart Saint <stuartsaint242@gmail.com>
2021-04-28 18:59:54 +0000

    Setup: FAQ page, no content

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """ Homepage """
     return render(request, 'home/index.html')
+
+
+def faq(request):
+    """ Frequently Asked Questions """
+    return render(request, 'home/faq.html')
